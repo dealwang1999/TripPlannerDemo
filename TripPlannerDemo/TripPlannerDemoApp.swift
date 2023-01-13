15c9db4732f78bf0f184e6b029831837553af8a9
dealwang <dealwang@ideaculture.com>
2023-01-13 15:40:02 +0800

    Initial Commit

---
+++ TripPlannerDemo/TripPlannerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerDemoApp.swift
+//  TripPlannerDemo
+//
+//  Created by dealwang on 2023/1/13.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
